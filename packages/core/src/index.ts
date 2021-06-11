727206f3c7ea5cf28de9c8d40e75679593bde924
mjzhang <mjzhang1993@163.com>
2021-06-11 19:21:58 +0800

    fix(core): fix rollup build config & export components interface (#45)
    affects: @acme-ui/core
    
    Co-authored-by: mjzhang1993 <mingjiazhang@yufuid.com>

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,8 +5,19 @@
 import './styles/cover/index.less';
 
 export { default as Pagination } from './components/Pagination';
+export type { PaginationProps } from './components/Pagination';
+
 export { default as Button } from './components/Button';
+export type { IButtonGroupProps, IButtonProps } from './components/Button';
+
 export { default as Switch } from './components/Switch';
+export type { SwitchProps } from './components/Switch';
+
 export { default as Radio } from './components/Radio';
+export type { IRadioProps, IRadioButtonProps, IRadioGroupProps } from './components/Radio';
+
 export { default as FormLabel } from './components/FormLabel';
+export type { FormLabelProps } from './components/FormLabel';
+
 export { default as FormGroup } from './components/FormGroup';
+export type { FormGroupProps } from './components/FormGroup';
