7f7632f04ed3b3543e11d23a87bbb9dfb9f4d491
xmliu <xiaominliu@yufuid.com>
2021-06-15 17:11:23 +0800

    feat(core): [Input] add some Input useing Icons
    affects: @acme-ui/core, @acme-ui/documents

--- packages/core/src/components/Input/Input.tsx
+++ packages/core/src/components/Input/Input.tsx
@@ -3,6 +3,9 @@ import get from 'lodash/get';
 import isString from 'lodash/isString';
 import isFunction from 'lodash/isFunction';
 import { PrimaryLoadingSvg } from '../Icon/LoadingIcon';
+import { SuccessSvg } from '../Icon/SuccessIcon';
+import { PasswordOpenEyeSvg, PasswordCloseEyeSvg } from '../Icon/PasswordEyeIcon';
+import { ClearSvg } from '../Icon/ClearIcon';
 import { uniteClassNames } from '../../utils/tools';
 import { InputMode, InputSize } from './types';
 import './styles/Input.less';
@@ -10,13 +13,15 @@ import './styles/Input.less';
 type InputModeType = `${InputMode}`;
 type InputSizeType = `${InputSize}`;
 
+/** TODO 数字类型的input 待设计给出详细设计和样式 */
+
 export interface IInputProps {
   /**
    * 样式类名
    */
   className?: string;
   /**
-   * input 类型
+   * input 类型 TODO 暂时没有样式变化
    */
   mode?: InputModeType;
   /**
@@ -26,11 +31,11 @@ export interface IInputProps {
   /**
    * 输入框值
    */
-  value?: string;
+  value?: React.InputHTMLAttributes<HTMLInputElement>['value'];
   /**
    * 输入框默认值 非受控
    */
-  defaultValue?: string;
+  defaultValue?: React.InputHTMLAttributes<HTMLInputElement>['defaultValue'];
   /**
    * 输入框内容提示
    */
@@ -66,7 +71,7 @@ export interface IInputProps {
   /**
    * 输入框内容变化时的回调
    */
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (e?: React.ChangeEvent<HTMLInputElement>) => void;
   /**
    * 输入框字数限制
    */
@@ -106,112 +111,139 @@ export const classes = {
   full: `${classNamePrefix}-full`,
   number: `${classNamePrefix}-number`,
   loading: `${classNamePrefix}-loading-icon`,
+  clear: `${classNamePrefix}-clear-icon`,
 };
 
-const Input = React.forwardRef((props: IInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
-  const {
-    className,
-    placeholder,
-    size,
-    limit,
-    error,
-    value,
-    onChange,
-    onFocus,
-    onBlur,
-    disabled,
-    fullWidth,
-    type,
-    success,
-    style,
-    loading,
-    startElement,
-    endElement,
-    ...otherProps
-  } = props;
+const Input: React.ForwardRefExoticComponent<IInputProps & React.RefAttributes<HTMLInputElement>> =
+  React.forwardRef((props: IInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
+    const {
+      className,
+      placeholder,
+      size,
+      limit,
+      error,
+      defaultValue,
+      value,
+      onChange,
+      onFocus,
+      onBlur,
+      disabled,
+      fullWidth,
+      type,
+      success,
+      style,
+      loading,
+      clear,
+      startElement,
+      endElement,
+      ...otherProps
+    } = props;
 
-  const [currentValue, setCurrentValue] = React.useState(0);
-  const propValueLength = isString(value) ? value.length : 0;
-  const [limitError, setLimitError] = React.useState(limit ? propValueLength > limit : false);
+    const [currentValue, setCurrentValue] = React.useState(defaultValue);
+    React.useEffect(() => {
+      setCurrentValue(value);
+    }, [value]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const internalValue = get(e, 'target.value');
-    const valueLength = isString(internalValue) ? internalValue.length : 0;
-    setCurrentValue(valueLength);
-    if (limit) {
-      setLimitError(valueLength > limit);
-    }
-    if (isFunction(onChange)) {
-      onChange(e);
-    }
-  };
+    const [currentValueLen, setCurrentValueLen] = React.useState(0);
+    const propValueLength = isString(value) ? value.length : 0;
+    const [limitError, setLimitError] = React.useState(limit ? propValueLength > limit : false);
 
-  const [isFocus, setIsFocus] = React.useState(false);
-  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (isFunction(onFocus)) {
-      onFocus(e);
-    }
-    setIsFocus(true);
-  };
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (isFunction(onBlur)) {
-      onBlur(e);
-    }
-    setIsFocus(false);
-  };
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const internalValue = get(e, 'target.value');
+      const valueLength = isString(internalValue) ? internalValue.length : 0;
+      setCurrentValueLen(valueLength);
+      setCurrentValue(internalValue);
+      if (limit) {
+        setLimitError(valueLength > limit);
+      }
+      if (isFunction(onChange)) {
+        onChange(e);
+      }
+    };
 
-  const [isPassword, setIsPassword] = React.useState(!!(type === 'password'));
-  const handlePasswordVisible = () => {
-    setIsPassword(!isPassword);
-  };
-  const passwordType = isPassword ? 'password' : 'text';
+    const [isFocus, setIsFocus] = React.useState(false);
+    const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+      if (isFunction(onFocus)) {
+        onFocus(e);
+      }
+      setIsFocus(true);
+    };
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+      if (isFunction(onBlur)) {
+        onBlur(e);
+      }
+      setIsFocus(false);
+    };
 
-  return (
-    <div
-      className={uniteClassNames(
-        classes.root,
-        isFocus ? classes.active : '',
-        success ? classes.success : '',
-        error || limitError ? classes.error : '',
-        disabled ? classes.disabled : '',
-        fullWidth ? classes.full : '',
-        type === 'number' ? classes.number : '',
-        className,
-      )}
-      style={style}
-      data-testid="input-root"
-    >
-      {startElement ? <div className={`${className}-start-element`}>{startElement}</div> : null}
-      <input
-        className={uniteClassNames(classes.input, classes.size(size))}
-        ref={ref}
-        placeholder={placeholder}
-        onChange={handleInputChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        disabled={disabled}
-        value={value}
-        type={type === 'password' ? passwordType : type}
-        {...otherProps}
-        data-testid="input"
-      />
-      {limit ? (
-        <div className={`${classNamePrefix}-limit`}>{`${currentValue}/${limit}`}</div>
-      ) : null}
-      {success ? <div className={`${classNamePrefix}-success-icon`}>V</div> : null}
-      {loading ? <PrimaryLoadingSvg className={`${classNamePrefix}-loading-icon`} /> : null}
-      {type === 'password' ? (
-        <div className={`${classNamePrefix}-password-icon`} onClick={handlePasswordVisible}>
-          {/** 需要替换文案为icon */}
-          {isPassword ? '睁眼' : '不睁眼'}
-        </div>
-      ) : null}
-      {endElement ? <div className={`${className}-end-element`}>{endElement}</div> : null}
-    </div>
-  );
-});
+    const [isPassword, setIsPassword] = React.useState(!!(type === 'password'));
+    const handlePasswordVisible = () => {
+      setIsPassword(!isPassword);
+    };
+    const passwordType = isPassword ? 'password' : 'text';
+
+    const handleInputClear = () => {
+      setCurrentValue('');
+      if (isFunction(onChange)) {
+        isFunction(onChange());
+      }
+    };
+
+    return (
+      <div
+        className={uniteClassNames(
+          classes.root,
+          isFocus ? classes.active : '',
+          success ? classes.success : '',
+          error || limitError ? classes.error : '',
+          disabled ? classes.disabled : '',
+          fullWidth ? classes.full : '',
+          type === 'number' ? classes.number : '',
+          className,
+        )}
+        style={style}
+        data-testid="input-root"
+      >
+        {startElement ? <div className={`${className}-start-element`}>{startElement}</div> : null}
+        <input
+          className={uniteClassNames(classes.input, classes.size(size))}
+          placeholder={placeholder}
+          onChange={handleInputChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          disabled={disabled}
+          value={currentValue}
+          type={type === 'password' ? passwordType : type}
+          {...otherProps}
+          data-testid="input"
+          ref={ref}
+        />
+        {clear ? (
+          <ClearSvg
+            className={uniteClassNames(
+              classes.clear,
+              currentValue ? `${classNamePrefix}-clear-appear` : '',
+            )}
+            onClick={handleInputClear}
+          />
+        ) : null}
+        {limit ? (
+          <div className={`${classNamePrefix}-limit`}>{`${currentValueLen}/${limit}`}</div>
+        ) : null}
+        {success ? <SuccessSvg /> : null}
+        {loading ? <PrimaryLoadingSvg className={classes.loading} /> : null}
+        {type === 'password' ? (
+          <div className={`${classNamePrefix}-password-icon`} onClick={handlePasswordVisible}>
+            {isPassword ? <PasswordOpenEyeSvg /> : <PasswordCloseEyeSvg />}
+          </div>
+        ) : null}
+        {endElement ? <div className={`${className}-end-element`}>{endElement}</div> : null}
+      </div>
+    );
+  });
 
 Input.defaultProps = {
+  defaultValue: '',
+  value: '',
   size: 'default',
   type: 'text',
 };
