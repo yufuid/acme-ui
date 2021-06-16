b1ce563dab089187f527f58b16b23d93303d58f2
gengfeiyang <1053221101@qq.com>
2021-06-16 09:55:34 +0800

    fix(core): [Checkbox] delete checkbox props onChnage value
    affects: @acme-ui/core

--- packages/core/src/components/Switch/__tests__/Switch.test.tsx
+++ packages/core/src/components/Switch/__tests__/Switch.test.tsx
@@ -1,46 +1,106 @@
-import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
-import { mountTestSuite } from 'tests/shared';
+import * as React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import { mountTestSuite, refTestSuite } from 'tests/shared';
+import userEvent from '@testing-library/user-event';
 import Switch, { classes, SwitchSize } from '../Switch';
+import { RefTestCaseType } from '../../../../tests/shared/refTest';
+
+const testId = 'switch-test-id';
 
 describe('✨️ Switch correctly', () => {
   mountTestSuite(<Switch />);
 
   test('render default switch', () => {
-    const { container } = render(<Switch />);
-    expect(container.firstChild).toHaveClass(classes.root);
-    expect(container.firstChild).toHaveClass(classes.size(SwitchSize.DEFAULT));
+    const { getByTestId } = render(<Switch data-testid={testId} />);
+    const acmeSwitch = getByTestId(testId);
+    expect(acmeSwitch).toHaveClass(classes.root);
+    expect(acmeSwitch).toHaveClass(classes.size(SwitchSize.DEFAULT));
   });
   test('render small switch', () => {
-    const { container } = render(<Switch size={SwitchSize.SMALL} />);
-    expect(container.firstChild).toHaveClass(classes.root);
-    expect(container.firstChild).toHaveClass(classes.size(SwitchSize.SMALL));
+    const { getByTestId } = render(<Switch data-testid={testId} size={SwitchSize.SMALL} />);
+    const acmeSwitch = getByTestId(testId);
+    expect(acmeSwitch).toHaveClass(classes.root);
+    expect(acmeSwitch).toHaveClass(classes.size(SwitchSize.SMALL));
   });
   test('render disabled switch', () => {
-    const { container } = render(<Switch loading />);
-    expect(container.firstChild).toHaveClass(classes.disabled);
-    const input = container.getElementsByTagName('input')[0];
-    expect(input).toBeDisabled();
+    const { getByTestId } = render(<Switch data-testid={testId} loading />);
+    const acmeSwitch = getByTestId(testId);
+    expect(acmeSwitch).toHaveClass(classes.disabled);
+    expect(acmeSwitch.querySelector('input')).toBeDisabled();
   });
   test('render loading switch', () => {
-    const { container } = render(<Switch loading />);
-    expect(container.firstChild).toHaveClass(classes.disabled);
-    const input = container.getElementsByTagName('input')[0];
-    const content = container.getElementsByClassName(classes.content)[0];
+    const { getByTestId } = render(<Switch data-testid={testId} loading />);
+    const acmeSwitch = getByTestId(testId);
+    expect(acmeSwitch).toHaveClass(classes.disabled);
+    const input = acmeSwitch.getElementsByTagName('input')[0];
+    const content = acmeSwitch.getElementsByClassName(classes.content)[0];
     expect(input).toBeDisabled();
     if (content) {
       const loading = content.getElementsByTagName('svg');
       expect(loading.length).toEqual(1);
     }
   });
-  test('switch change correctly', () => {
-    const { container } = render(<Switch loading />);
-    const root = container.firstChild;
-    expect(root).not.toBeNull();
-    if (root) {
-      const input = container.getElementsByTagName('input')[0];
-      fireEvent.change(input, { target: { checked: true } });
-      expect(input.checked).toEqual(true);
-    }
+
+  describe('test Event correctly', () => {
+    test('on default checkbox', () => {
+      let target = null;
+      let checked = false;
+      const mockFn = jest.fn((e) => {
+        target = e.target;
+        checked = target.checked;
+      });
+      const { getByTestId } = render(<Switch data-testid={testId} onChange={mockFn} />);
+      const acmeSwitch = getByTestId(testId);
+      userEvent.click(acmeSwitch);
+      expect(target).toEqual(expect.any(HTMLInputElement));
+      expect(checked).toEqual(true);
+      expect(acmeSwitch.querySelector('input')).toBeChecked();
+    });
+  });
+
+  refTestSuite('test ref correctly', {
+    [RefTestCaseType.createRef]: () => {
+      const elementRef = React.createRef<HTMLLabelElement>();
+      const mockFn = jest.fn(() => {});
+
+      render(<Switch ref={elementRef} onChange={mockFn} />);
+
+      expect(elementRef.current).toBeInTheDocument();
+
+      expect(elementRef.current).toEqual(expect.any(HTMLLabelElement));
+
+      if (elementRef.current) {
+        userEvent.click(elementRef.current);
+      }
+      expect(mockFn).toBeCalledTimes(1);
+    },
+    [RefTestCaseType.callback]: () => {
+      let elementRef: HTMLLabelElement | null = null;
+      const SwitchRefCallback = (ref: HTMLLabelElement) => {
+        elementRef = ref;
+      };
+
+      render(<Switch ref={SwitchRefCallback} />);
+
+      expect(elementRef).toBeInTheDocument();
+
+      expect(elementRef).toEqual(expect.any(HTMLLabelElement));
+    },
+    [RefTestCaseType.useRef]: async () => {
+      let elementRef: React.RefObject<HTMLLabelElement>;
+      const Test = () => {
+        elementRef = React.useRef<HTMLLabelElement>(null);
+
+        return <Switch ref={elementRef} />;
+      };
+
+      await waitFor(() => {
+        render(<Test />);
+
+        expect(elementRef.current).toBeInTheDocument();
+
+        expect(elementRef.current).toEqual(expect.any(HTMLLabelElement));
+      });
+    },
   });
 });
